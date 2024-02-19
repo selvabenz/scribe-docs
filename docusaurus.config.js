bc2d8baf65bdc0a24088f0cd244da49e050718fa
Benz <benzmcc@gmail.com>
2024-02-19 09:50:12 +0530

    url updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Scribe-docs',
   tagline: 'Documentation for Scribe',
-  url: 'https://autographa-docs.netlify.app/',
+  url: 'https://selvabenz.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
